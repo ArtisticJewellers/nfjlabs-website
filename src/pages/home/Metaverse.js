cf0dacd211e664bd98739f3b531c77cf111a8a0a
AnandJNairGit <anandjnairit@gmail.com>
2023-04-28 13:06:39 +0530

    add metaverse link

---
+++ src/pages/home/Metaverse.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import backgroundImage from "../../assets/homePage/metaverseBackground.png";
+import { Box, Typography } from "@mui/material";
+import Centered from "../../components/common/Centered";
+
+const Metaverse = () => {
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        paddingY: { xs: "20px", sm: "25px", md: "30px" },
+        marginY: { xs: "30px", sm: "60px", md: "90px" }
+      }}
+    >
+      <Centered>
+        <Typography sx={{ fontWeight: 500, fontSize: { xs: "15px", sm: "18px", md: "20px" }, color: "#734b21" }}>
+          Check our{" "}
+          <a href="https://example.com/metaverse" style={{ color: "#734b21" }}>
+            Metaverse
+          </a>
+        </Typography>
+      </Centered>
+    </Box>
+  );
+};
+
+export default Metaverse;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./banner";
 import Artists from "./artists";
 import Marketplace from "./marketplace";
+import Metaverse from "./Metaverse";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <Artists />
       <Marketplace />
+      <Metaverse />
     </>
   );
 };
