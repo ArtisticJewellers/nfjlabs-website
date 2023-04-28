d624be1875d360e757e3013cbc53eb20648d8bd4
AnandJNairGit <anandjnairit@gmail.com>
2023-04-28 11:55:40 +0530

    make the out arrow responsive

--- src/components/common/OutArrowBtn.js
+++ src/components/common/OutArrowBtn.js
@@ -5,7 +5,7 @@ const OutArrowBtn = ({ label }) => {
   return (
     <>
       <Button
-        sx={{ color: "black", borderColor: "black" }}
+        sx={{ color: "black", borderColor: "black", fontSize:{xs:"10px", sm:"15px", md:"19px"} }}
         variant="outlined"
         endIcon={<ArrowOutwardIcon />}
       >
