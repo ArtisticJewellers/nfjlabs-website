3a8bcd7242a3fc64668e99f2fac32a395908ad44
AnandJNairGit <anandjnairit@gmail.com>
2023-04-28 12:00:27 +0530

    enable gradient text component to render heading

--- src/components/common/GradientText.js
+++ src/components/common/GradientText.js
@@ -1,18 +1,29 @@
 import { styled } from "@mui/material/styles";
 import { Typography } from "@mui/material";
 
-const GradientText = ({ text, variant, titleW, titleM }) => {
+const GradientText = ({
+  text,
+  variant,
+  titleW,
+  titleM,
+  headingW,
+  headingM,
+}) => {
   const GradientTypography = styled(Typography)(({ theme }) => ({
     backgroundImage:
       "linear-gradient(90deg, #000000 -4.69%, #ECA830 48.02%, #000000 103.89%)",
     backgroundClip: "text",
     color: "transparent",
-    fontWeight: titleM || titleW ? 400 : 700,
-    fontFamily: titleM || titleW ? "" : "Cormorant",
+    fontWeight: titleM || titleW || headingW || headingM ? 400 : 700,
+    fontFamily: titleM || titleW || headingW || headingM ? "" : "Cormorant",
   }));
 
   return (
-    <GradientTypography variant={titleW ? "h2" : titleM ? "h4" : "h4"}>
+    <GradientTypography
+      variant={
+        titleW ? "h2" : titleM ? "h4" : headingW ? "h3" : headingM ? "h6" : "h4"
+      }
+    >
       {text}
     </GradientTypography>
   );
