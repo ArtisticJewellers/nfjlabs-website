57214fdfda560d01a4097d9bc53748218fee1415
AnandJNairGit <anandjnairit@gmail.com>
2023-05-04 12:04:31 +0530

    add slider section to blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Slider from "./Slider.js";
+import { Box } from "@mui/material";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <>
+      <Box sx={{mt:10}} />
+      <Slider />
+    </>
+  );
 };
 
 export default Blog;
