cbaa983b2960d8a79a9a59043e7ecc66f3d53f48
AnandJNairGit <anandjnairit@gmail.com>
2023-05-04 23:53:39 +0530

    add top stories section

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Slider from "./Slider/index.js";
 import { Box } from "@mui/material";
+import TopStories from "./topStories/index.js";
 
 const Blog = () => {
   return (
     <>
       <Box sx={{mt:10}} />
       <Slider />
+      <TopStories />
     </>
   );
 };
