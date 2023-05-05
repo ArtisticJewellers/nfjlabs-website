fa9724c06a7f1a9b2c93e18efcec0e8194f55935
AnandJNairGit <anandjnairit@gmail.com>
2023-05-05 13:28:42 +0530

    fix the size issue in avatar component

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -3,7 +3,14 @@ import React from "react";
 
 const Avatar = ({ size, text, img }) => {
   return (
-    <MuiAvatar src={img} sx={{ height: size, width: size, fontSize: size / 2 }}>
+    <MuiAvatar
+      src={img}
+      sx={{
+        height: Math.round(size),
+        width: Math.round(size),
+        fontSize: `${Math.round(size / 2)}px`,
+      }}
+    >
       {text ? text : ""}
     </MuiAvatar>
   );
