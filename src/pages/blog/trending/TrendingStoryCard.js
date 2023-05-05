730565bc309d672fea92866bbecb5e84d901392e
AnandJNairGit <anandjnairit@gmail.com>
2023-05-05 17:57:43 +0530

    create trending story card

---
+++ src/pages/blog/trending/TrendingStoryCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Row from "../../../components/common/Row";
+import { Box, Divider, Typography } from "@mui/material";
+
+const TrendingStoryCard = ({ title, description, timeAgo, views, image }) => {
+  return (
+    <>
+      <Row sx={{ margin: 2 }}>
+        <img
+          src={image}
+          height="72px"
+          width="72px"
+          style={{ borderRadius: "5px" }}
+        />
+        <Box sx={{ width: "70%" }}>
+          <Row>
+            <Typography
+              sx={{ fontSize: "10px" }}
+            >{`${views} views`}</Typography>
+            <Typography sx={{ fontSize: "10px" }}>{`${timeAgo}`}</Typography>
+          </Row>
+          <Typography
+            sx={{ fontSize: "14px", fontWeight: 600, marginTop: "5px" }}
+          >
+            {title}
+          </Typography>
+        </Box>
+      </Row>
+      <Divider />
+    </>
+  );
+};
+
+export default TrendingStoryCard;
