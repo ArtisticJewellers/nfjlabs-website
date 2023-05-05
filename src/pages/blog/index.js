fd983b88f922d3169903dcc76d6cc71bd05de2b6
AnandJNairGit <anandjnairit@gmail.com>
2023-05-05 13:31:19 +0530

    add aditional stories section

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import Slider from "./Slider/index.js";
 import { Box } from "@mui/material";
 import TopStories from "./topStories/index.js";
+import AditionalStoriesLayout from "./AditionalStoriesLayout.js";
 
 const Blog = () => {
   return (
     <>
-      <Box sx={{mt:10}} />
+      <Box sx={{ mt: 10 }} />
       <Slider />
       <TopStories />
+      <AditionalStoriesLayout />
     </>
   );
 };
