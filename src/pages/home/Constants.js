f8a8d0ddf0c9e7881a918e37b2d330519a20e32d
AnandJNairGit <anandjnairit@gmail.com>
2023-05-08 22:13:54 +0530

    add faq content

---
+++ src/pages/home/Constants.js
@@ -0,0 +1,35 @@
+const links = {
+  artists: "http://nfjlabs.io/#/artists",
+  marketplace: "http://nfjlabs.io/#/explore",
+  metaverse: "https://mv.ibentos.com/rnd/artGallary2022/",
+};
+
+const faqs = [
+  {
+    question: "What is NFT jewellery?",
+    answer:
+      "NFT jewellery is physical jewellery represented by a non-fungible token (NFT) on a blockchain. The NFT verifies ownership and authenticity, and can record additional information. NFT jewellery allows for secure and transparent ownership, and creates a new market for collectible digital jewellery.",
+  },
+  {
+    question: "How are the NFTs used in Art world?",
+    answer:
+      "NFTs are used in the art world to create unique digital assets that can be sold and traded on a blockchain. Artists can create digital art pieces that are verified by NFTs, which ensure authenticity and ownership. The use of NFTs also allows for more transparent and secure transactions in the art world. NFTs can also provide a new revenue stream for artists, who can sell their digital art pieces directly to collectors.",
+  },
+  {
+    question: "How is NFT Jewellery stored?",
+    answer:
+      "To store an NFT, the owner of the jewellery must create a digital wallet that is compatible with the blockchain where the NFT will be stored. The digital wallet stores the private keys that allow the owner to access and manage their NFT. The NFT itself is stored on the blockchain, which can be accessed and viewed through a digital marketplace or wallet. ",
+  },
+  {
+    question: "How do i buy NFT jewellery?",
+    answer:
+      "To buy NFT jewellery, you first need to create a digital wallet that is compatible with the blockchain where the NFT is stored. You can then browse NFT marketplaces, which offer a variety of NFT jewellery pieces. Once you find a piece of NFT jewellery you like, you can bid on it or buy it outright using cryptocurrency. The NFT will be transferred to your digital wallet, which verifies your ownership and authenticity of the physical jewellery piece. It's important to research the reputation of the seller and the authenticity of the jewellery before making a purchase.",
+  },
+  {
+    question: "How does the value of NFT jewellery change? ",
+    answer:
+      "The value of NFT jewellery is determined by various factors such as the uniqueness of the design, the materials used, the reputation of the artist/designer, and the demand in the market. As with any collectible, the value can fluctuate based on supply and demand, as well as changes in the market. Additionally, the value of the cryptocurrency used to purchase the NFT can also impact its value.",
+  },
+];
+
+export { links, faqs };
--- src/pages/home/faq/index.js
+++ src/pages/home/faq/index.js
@@ -3,25 +3,14 @@ import AccordionContent from "./AccordionContent";
 import { Box, Container, Typography } from "@mui/material";
 import Centered from "../../../components/common/Centered";
 import backgroundImage from "../../../assets/homePage/Star.png";
+import { faqs } from "../Constants";
 
 const FAQ = () => {
-  const dummyText =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam faucibus sem at ullamcorper rhoncus. Sed finibus tellus non mi iaculis, vel aliquet quam elementum. Aenean efficitur, sapien in viverra posuere, magna sapien iaculis augue, vitae fermentum tortor arcu vel velit. Nam lacinia pretium urna vel tempus.";
-  const faqs = [
-    { question: "What is NFT Jewellery?", answer: dummyText },
-    { question: "How are NFTs used in the art world?", answer: dummyText },
-    { question: "How is NFT jewelry stored?", answer: dummyText },
-    { question: "How do I buy NFT jewelry?", answer: dummyText },
-    {
-      question: "How does the value of NFT jewelry change?",
-      answer: dummyText,
-    },
-  ];
   const DisplayAnswer = ({ answer }) => {
     return (
       <Typography
         sx={{
-          width: "70%",
+          // width: "90%",
           color: "#808080",
           marginY: "10px",
           fontSize: { xs: "12px", sm: "15px", md: "20px" },
