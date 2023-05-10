c45f653ba693f13b065561b2f38fb7a01015f279
AnandJNairGit <anandjnairit@gmail.com>
2023-05-10 11:17:34 +0530

    display the animation text in a single line

--- src/pages/home/banner/TypeAnimation.js
+++ src/pages/home/banner/TypeAnimation.js
@@ -4,33 +4,41 @@ import Typewriter from "typewriter-effect";
 
 const TypeAnimation = () => {
   return (
-    <>
-      <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex" }}>
+      <Typography
+        variant="h1"
+        sx={{
+          display: "flex", // add this line
+          alignItems: "center", // add this line
+          backgroundImage:
+            "linear-gradient(90deg, #000000 -4.69%, #ECA830 48.02%, #000000 103.89%)",
+          backgroundClip: "text",
+          color: "transparent",
+          fontWeight: 400,
+          fontSize: { xs: "25px", sm: "35px", md: "65px" },
+        }}
+      >
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+            delay: 50,
+            strings: ["Jewellery", "Gems", "Coins"],
+            pauseFor: 2000,
+            cursorClassName: "Typewriter__cursor",
+          }}
+        />
         <Typography
           variant="h1"
           sx={{
-            backgroundImage:
-              "linear-gradient(90deg, #000000 -4.69%, #ECA830 48.02%, #000000 103.89%)",
-            backgroundClip: "text",
-            color: "transparent",
             fontWeight: 400,
-            fontSize: { xs: "33px", sm: "35px", md: "65px" },
+            fontSize: { xs: "25px", sm: "35px", md: "65px" },
           }}
         >
-          <Typewriter
-            options={{
-              autoStart: true,
-              loop: true,
-              delay: 50,
-              strings: ["Jewellery", "Gems", "Coins"],
-              pauseFor: 2000,
-              cursorClassName: "Typewriter__cursor",
-            }}
-          />
-          {"paired with NFTs"}
+          paired with NFTs
         </Typography>
-      </Box>
-    </>
+      </Typography>
+    </Box>
   );
 };
 
