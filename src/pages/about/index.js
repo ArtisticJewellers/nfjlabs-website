2eb4894382fb2df90086aa977aabe57952e6caa1
AnandJNairGit <anandjnairit@gmail.com>
2023-05-22 14:17:00 +0530

    add milestones icons

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import OurVision from "./vision";
 import { Box } from "@mui/material";
+import Milestones from "./milestones";
 
 const About = () => {
   return <>
   <Box sx={{ mt: 10 }} />
 
   <OurVision />
+  <Milestones />
   </>
 };
 
