e1c740ccc31f9c044554952950ac28fc3beb9d08
AnandJNairGit <anandjnairit@gmail.com>
2023-06-20 16:35:46 +0530

    update visions in about page

--- src/pages/about/vision/index.js
+++ src/pages/about/vision/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Centered from "../../../components/common/Centered";
 import Headding from "../../../components/common/Headding";
 import Visions from "./Visions";
+import backgroundImage from "../../../assets/about/visions/background.png"
 
 const OurVision = () => {
   return (
@@ -10,18 +11,21 @@ const OurVision = () => {
       <Box
         sx={{
           paddingY: 5,
-          background:
-            "linear-gradient(106.61deg, rgba(255, 218, 164, 0.19) 8.3%, #FFF9EF 28.06%, rgba(255, 218, 164, 0.13) 48.66%, #FFF9EF 71.79%, rgba(255, 218, 164, 0.19) 89.02%)",
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
         }}
       >
         <Container>
           <Centered>
-            <Headding text="Our Vision" />
+            <Headding color="#ffff" text="Our Vision" />
             <Typography
               sx={{
                 marginY: 3,
                 textAlign: "center",
                 fontSize: { xs: "10px", sm: "15px", md: "17px" },
+                color:"#ffff"
               }}
             >
               Our vision is to redefine the jewellery industry, merging
